d215b4969cfb0cd486712ed1e76bd4c61a76e12c
Aron Bordin <aron.bordin@gmail.com>
2016-10-29 18:14:47 -0200

    fix power

--- lib/THNN/generic/PPS.c
+++ lib/THNN/generic/PPS.c
@@ -12,40 +12,41 @@ void THNN_(PPS_updateOutput)(
 
     TH_TENSOR_APPLY2(real, output, real, input,
         real s = 1./(1.+ exp(- *input_data));
-        *output_data = s;
+        real sig = s;
+        *output_data = sig;
         if (order > 0) {
-            s *= s;
+            s *= sig;
         }
 
         if (order == 1) {
             *output_data += -s;
         } else if (order == 2) {
             *output_data += -3*s;
-            s *= s;
+            s *= sig;
             *output_data += 2*s;
         } else if (order == 3) {
             *output_data += -7*s;
-            s *= s;
+            s *= sig;
             *output_data += 12*s;
-            s *= s;
+            s *= sig;
             *output_data += -6*s;
         } else if (order == 4) {
             *output_data += -15*s;
-            s *= s;
+            s *= sig;
             *output_data += 50*s;
-            s *= s;
+            s *= sig;
             *output_data += -60*s;
-            s *= s;
+            s *= sig;
             *output_data += 24*s;
         } else if (order == 5) {
             *output_data += -31*s;
-            s *= s;
+            s *= sig;
             *output_data += 180*s;
-            s *= s;
+            s *= sig;
             *output_data += -390*s;
-            s *= s;
+            s *= sig;
             *output_data += 360*s;
-            s *= s;
+            s *= sig;
             *output_data += -120*s;
         }
     );
@@ -63,37 +64,38 @@ void THNN_(PPS_updateGradInput)(
     THTensor_(resizeAs)(gradInput, output);
     TH_TENSOR_APPLY3(real, gradInput, real, gradOutput, real, input,
         real s = 1./(1.+ exp(- *input_data));
+        float sig = s;
         real derivative = s;
         s *= s;
         if (order == 1) {
             derivative += -s;
         } else if (order == 2) {
             derivative += -3*s;
-            s *= s;
+            s *= sig;
             derivative += 2*s;
         } else if (order == 3) {
             derivative += -7*s;
-            s *= s;
+            s *= sig;
             derivative += 12*s;
-            s *= s;
+            s *= sig;
             derivative += -6*s;
         } else if (order == 4) {
             derivative += -15*s;
-            s *= s;
+            s *= sig;
             derivative += 50*s;
-            s *= s;
+            s *= sig;
             derivative += -60*s;
-            s *= s;
+            s *= sig;
             derivative += 24*s;
         } else if (order == 5) {
             derivative += -31*s;
-            s *= s;
+            s *= sig;
             derivative += 180*s;
-            s *= s;
+            s *= sig;
             derivative += -390*s;
-            s *= s;
+            s *= sig;
             derivative += 360*s;
-            s *= s;
+            s *= sig;
             derivative += -120*s;
         }
         *gradInput_data = *gradOutput_data * derivative;
